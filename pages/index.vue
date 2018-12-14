45ab9104e609c8d4f2f7d3180033267baf59d3e3
OperKH <oper.kh@gmail.com>
2018-12-14 18:05:38 +0200

    Initial.

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="text-left">text-left</div>
+    <div class="text-center">text-center</div>
+    <div class="text-right">text-right</div>
+    <div class="text-start">text-start</div>
+    <div class="text-end">text-end</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BugExample"
+};
+</script>
+
+<style>
+.text-left {
+  text-align: left;
+}
+.text-center {
+  text-align: center;
+}
+.text-right {
+  text-align: right;
+}
+.text-start {
+  text-align: start;
+}
+.text-end {
+  text-align: end;
+}
+</style>
